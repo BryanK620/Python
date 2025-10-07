fec32e344dd05764d7282d8d80e302f4523e8d5f
Bryan Kássio <bryankassio80@gmail.com>
2025-10-07 14:02:24 -0300

    Update identacao.py

--- identacao.py
+++ identacao.py
@@ -2,9 +2,11 @@ def sacar(valor):
     saldo = 500
 
     if saldo >= valor:
-        print('valor sacado!')
-        print('retire o seu dinheiro na boca do caixa.')
+        print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa.")
 
-    print('Obrigado por ser nosso cliente, tenha um bom dia!')
+        print ("Obrigado por ser nosso cliente, tenha um bom dia!")
+
+    sacar(100)
 
 
